58f6866cdda20aba25e2ee0906bc63f71913f366
Jarod <jarodf@sfu.ca>
2020-08-22 21:38:42 -0700

    Update clock.py so it can be called from the interpreter

--- clock.py
+++ clock.py
@@ -2,18 +2,24 @@ import time
 import datetime
 import serial
 
-# Initialize serial USB connection
-ser = serial.Serial('COM5', 9600)
 
-# Send the time
-while True:
-    # Get the current time
-    now = datetime.datetime.now()
-    current_time = now.strftime("%I:%M:%S")
+def send_time():
+    # Initialize serial USB connection
+    ser = serial.Serial('COM5', 9600)
 
-    # Send the current time to the arduino
-    ser.write(bytes(current_time, encoding="utf-8"))
-    print("\"" + current_time + "\" was sent to arduino.")
-    
-    # Wait so we don't spam the arduino
-    time.sleep(.5)
+    # Send the time
+    while True:
+        # Get the current time
+        now = datetime.datetime.now()
+        current_time = now.strftime("%I:%M:%S")
+
+        # Send the current time to the arduino
+        ser.write(bytes(current_time, encoding="utf-8"))
+        print("\"" + current_time + "\" was sent to arduino.")
+        
+        # Wait so we don't spam the arduino
+        time.sleep(.5)
+
+
+if __name__ == '__main__':
+    send_time()
